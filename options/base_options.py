ef08652b771b4543a2d6e037f4f013b0a9eb73ad
Beksultan <94361535+Beksultanden@users.noreply.github.com>
2026-02-21 08:36:23 +0300

    Add comments to BaseOptions for parameter clarification
    Added comments in Russian to clarify the purpose of various parameters and sections in the BaseOptions class.

--- options/base_options.py
+++ options/base_options.py
@@ -7,10 +7,13 @@ import data
 
 
 class BaseOptions():
+    # класс который отвечает за обработку всех параметров запуска работы, настраиваются модель, датасет, гиперпараметры,
+    # GPU, путь сохранения и параметры цветизации
     def __init__(self):
         self.initialized = False
 
     def initialize(self, parser):
+         # Основные параметры обучения и модели
         parser.add_argument('--batch_size', type=int, default=25, help='input batch size')
         parser.add_argument('--loadSize', type=int, default=256, help='scale images to this size')
         parser.add_argument('--fineSize', type=int, default=256, help='then crop to this size')
@@ -22,10 +25,13 @@ class BaseOptions():
         parser.add_argument('--which_model_netG', type=str, default='siggraph', help='selects model to use for netG')
         parser.add_argument('--n_layers_D', type=int, default=3, help='only used if which_model_netD==n_layers')
         parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+        # Настройки датасета
         parser.add_argument('--dataset_mode', type=str, default='aligned', help='chooses how datasets are loaded. [unaligned | aligned | single]')
         parser.add_argument('--which_direction', type=str, default='AtoB', help='AtoB or BtoA')
         parser.add_argument('--nThreads', default=4, type=int, help='# threads for loading data')
+        # Пути и сохранение
         parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
+        # Нормализация и аугментации
         parser.add_argument('--norm', type=str, default='batch', help='instance normalization or batch normalization')
         parser.add_argument('--serial_batches', action='store_true', help='if true, takes images in order to make batches, otherwise takes them randomly')
         parser.add_argument('--display_winsize', type=int, default=256, help='display window size')
@@ -37,9 +43,11 @@ class BaseOptions():
                             help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
         parser.add_argument('--resize_or_crop', type=str, default='resize_and_crop', help='scaling and cropping of images at load time [resize_and_crop|crop|scale_width|scale_width_and_crop]')
         parser.add_argument('--no_flip', action='store_true', help='if specified, do not flip the images for data augmentation')
+        # Инициализация сети
         parser.add_argument('--init_type', type=str, default='normal', help='network initialization [normal|xavier|kaiming|orthogonal]')
         parser.add_argument('--verbose', action='store_true', help='if specified, print more debugging information')
         parser.add_argument('--suffix', default='', type=str, help='customized suffix: opt.name = opt.name + suffix: e.g., {model}_{which_model_netG}_size{loadSize}')
+        # Параметры цветизации пространство Lab 
         parser.add_argument('--ab_norm', type=float, default=110., help='colorization normalization factor')
         parser.add_argument('--ab_max', type=float, default=110., help='maximimum ab value')
         parser.add_argument('--ab_quant', type=float, default=10., help='quantization factor')
@@ -48,7 +56,7 @@ class BaseOptions():
         parser.add_argument('--mask_cent', type=float, default=.5, help='mask centering factor')
         parser.add_argument('--sample_p', type=float, default=1.0, help='sampling geometric distribution, 1.0 means no hints')
         parser.add_argument('--sample_Ps', type=int, nargs='+', default=[1, 2, 3, 4, 5, 6, 7, 8, 9, ], help='patch sizes')
-
+        # Доп параметры тестирования
         parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
         parser.add_argument('--classification', action='store_true', help='backprop trunk using classification, otherwise use regression')
         parser.add_argument('--phase', type=str, default='val', help='train_small, train, val, test, etc')
